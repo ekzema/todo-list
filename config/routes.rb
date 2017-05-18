9316f09eaecc16a6144816f22ccc7c46404456ef
ekzema <xxx.zp@mail.ru>
2017-05-18 16:24:33 +0300

    'test'

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+
+  devise_for :users, :controllers => {
+      :omniauth_callbacks => "users/omniauth_callbacks"
+  }do
+
+
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+
+
+end
+end
